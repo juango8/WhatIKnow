41b1683bcbd11cc25b688c55a77f94843c877809
Juango Flores <jgonzalofh2898@gmail.com>
2022-09-08 23:03:06 -0500

    :tada: Setting default template

---
+++ app/src/main/java/com/juango/talanachallenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.juango.talanachallenge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.juango.talanachallenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
